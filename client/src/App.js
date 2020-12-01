d991e5e0579b0c93d9d32d3aa90d79ced1452385
Gatta Loukik <loukik.gatta@gmail.com>
2020-12-02 01:50:17 +0530

    New Features Updated

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,24 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 const crypto = require("crypto");
 
+const GetAllBankAccounts = (props) => {
+  if (parseInt(props.bankcount) > 0) {
+    return (
+      <div>
+        {props.banks.map((bank) => (
+          <p key={bank.key}>{bank.address}</p>
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>There are no verified Bank Accounts in this network!</p>
+      </div>
+    );
+  }
+};
+
 class App extends Component {
   state = {
     web3: null,
@@ -16,6 +34,12 @@ class App extends Component {
     data_hash: null,
     b_name: null,
     bank_verify: null,
+    entity: null,
+    allaccounts: null,
+    allbanks: [],
+    bank_count: 0,
+    status: null,
+    requestAddress: null,
   };
 
   componentDidMount = async () => {
@@ -25,7 +49,6 @@ class App extends Component {
 
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
-
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = KycBlockChain.networks[networkId];
@@ -36,7 +59,15 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, account: accounts[0], contract: instance });
+      this.setState({
+        web3,
+        account: accounts[0],
+        contract: instance,
+        allaccounts: accounts,
+      });
+      this.whoami();
+      this.numbanks();
+      // this.GetAllBankAccounts();
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -68,8 +99,24 @@ class App extends Component {
     this.setState({ bank_verify: event.target.value });
   };
 
-  createmycustomer = async (e) => {
-    e.preventDefault();
+  requestAddressChange = (event) => {
+    this.setState({ requestAddress: event.target.value });
+  };
+
+  whoami = async () => {
+    var { contract } = this.state;
+    const cus = await contract.methods
+      .isCus()
+      .call({ from: this.state.account });
+    const org = await contract.methods
+      .isOrg()
+      .call({ from: this.state.account });
+
+    var who = cus ? "Customer" : org ? "Bank" : "None";
+    this.setState({ entity: who });
+  };
+
+  createmycustomer = async () => {
     var { contract } = this.state;
     await contract.methods
       .newCustomer(
@@ -83,8 +130,7 @@ class App extends Component {
       .send({ from: this.state.account });
   };
 
-  createmybank = async (e) => {
-    e.preventDefault();
+  createmybank = async () => {
     var { contract } = this.state;
 
     await contract.methods
@@ -101,71 +147,299 @@ class App extends Component {
     this.setState({ data_hash: response });
   };
 
+  get = async () => {
+    var { contract } = this.state;
+    var access = await contract.methods
+      .isOrg()
+      .call({ from: this.state.account });
+    if (access) {
+      this.getkycfromcustomer();
+    } else {
+      window.alert("You are not a verified Bank!");
+    }
+  };
+
+  create_customer = async (e) => {
+    e.preventDefault();
+    var { contract } = this.state;
+    var access = await contract.methods
+      .isCus()
+      .call({ from: this.state.account })
+      .then(
+        () => {
+          window.alert("You successfully made an account!");
+        },
+        () => {
+          window.alert("You already have an account!");
+        }
+      );
+    if (!access) {
+      this.createmycustomer();
+      this.whoami();
+    }
+  };
+
+  create_bank = async (e) => {
+    e.preventDefault();
+    var { contract } = this.state;
+    var access = await contract.methods
+      .isOrg()
+      .call({ from: this.state.account })
+      .then(
+        () => {
+          window.alert("You are now a verified Bank Entity!");
+        },
+        () => {
+          window.alert("You already are a bank!");
+        }
+      );
+    if (!access) {
+      this.createmybank();
+      this.whoami();
+    }
+  };
+
+  modify_data = async (e) => {
+    e.preventDefault();
+    var { contract } = this.state;
+    var confirm = await contract.methods
+      .isCus()
+      .call({ from: this.state.account });
+    if (confirm) {
+      await contract.methods
+        .modifyCustomerData(
+          this.state.name,
+          crypto
+            .createHash("sha1")
+            .update(this.state.name + this.state.aadhar + this.state.pan)
+            .digest("hex"),
+          this.state.bank_verify
+        )
+        .send({ from: this.state.account })
+        .then(() => {
+          window.alert("Data Changed!");
+        });
+    }
+  };
+
+  numbanks = async () => {
+    var { contract } = this.state;
+    var len = await contract.methods.bankslength().call();
+    this.setState({ bank_count: len });
+    var banks = [];
+    if (parseInt(this.state.bank_count) > 0) {
+      for (var i = 0; i < len; i++) {
+        banks.push({
+          key: i,
+          address: await contract.methods.Banks(i).call(),
+        });
+      }
+    }
+
+    this.setState({ allbanks: banks });
+  };
+
+  getmystatus = async () => {
+    var { contract } = this.state;
+    var status = await contract.methods
+      .checkStatus()
+      .call({ from: this.state.account });
+
+    if (status === "0") {
+      this.setState({ status: "Accepted" });
+    } else if (status === "1") {
+      this.setState({ status: "Rejected" });
+    } else if (status === "2") {
+      this.setState({ status: "Pending" });
+    } else {
+      this.setState({ status: "Undefined" });
+    }
+  };
+
+  viewRequests = async () => {
+    var { contract } = this.state;
+    var reqs = await contract.methods.viewRequests().call({
+      from: this.state.account,
+    });
+    console.log(reqs);
+  };
+
+  accept = async () => {
+    var { contract } = this.state;
+    await contract.methods
+      .changeStatusToAccepted(this.state.requestAddress)
+      .send({ from: this.state.account })
+      .then(
+        () => {
+          window.alert("Status Changed!");
+        },
+        () => {
+          window.alert("You are not authorized!");
+        }
+      );
+  };
+
+  reject = async () => {
+    var { contract } = this.state;
+    await contract.methods
+      .changeStatusToRejected(this.state.requestAddress)
+      .send({ from: this.state.account })
+      .then(
+        () => {
+          window.alert("Status Changed!");
+        },
+        () => {
+          window.alert("You are not authorized!");
+        }
+      );
+  };
+
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h2>Current Account</h2>
-        <p>{this.state.account}</p>
-        <form
-          action="."
-          method=""
-          class="form"
-          onSubmit={this.createmycustomer}
-        >
+        <h2>Current Account is a {this.state.entity} entity</h2>
+        <h3>{this.state.account}</h3>
+        <fieldset>
+          <legend>
+            <strong>Verified Bank Addresses</strong>
+          </legend>
+          <GetAllBankAccounts
+            bankcount={this.state.bank_count}
+            banks={this.state.allbanks}
+          />
+        </fieldset>
+
+        <br />
+        <br />
+
+        <form action="." method="" onSubmit={this.create_customer}>
           <fieldset>
-            <legend>I am a Customer</legend>
+            <legend>
+              <strong>Customer Registration Form</strong>
+            </legend>
             <p>
-              <label>Customer Name</label>
+              <label>Your Name </label>
               <input type="text" onChange={this.myNameChangeHandler} />
             </p>
 
             <p>
-              <label>Customer Aadhar</label>
+              <label>Your Aadhar </label>
               <input type="text" onChange={this.myAadharChangeHandler} />
             </p>
 
             <p>
-              <label>Customer Pan</label>
+              <label>Your Pan </label>
               <input type="text" onChange={this.myPanChangeHandler} />
             </p>
 
             <p>
-              <label>Bank Verify</label>
+              <label>Bank Address you want to verify your data with </label>
               <input type="text" onChange={this.myVBankChangeHandler} />
             </p>
+            <p>
+              <input type="submit" name="submit" value="Create Customer" />
+            </p>
           </fieldset>
-
-          <p>
-            <input type="submit" name="submit" value="Save" class="button" />
-          </p>
         </form>
 
-        <form action="." method="" class="form" onSubmit={this.createmybank}>
+        <br />
+        <br />
+
+        <form action="." method="" onSubmit={this.create_bank}>
           <fieldset>
-            <legend>I am a Bank</legend>
+            <legend>
+              <strong>Bank Registration Form</strong>
+            </legend>
             <p>
-              <label>Bank Name</label>
+              <label>Bank Name </label>
               <input type="text" onChange={this.myBankNameChangeHandler} />
             </p>
+            <p>
+              <input type="submit" name="submit" value="Create bank" />
+            </p>
           </fieldset>
-
-          <p>
-            <input type="submit" name="submit" value="Save" class="button" />
-          </p>
         </form>
 
-        <div>
-          <p>
-            <label>View KYC data</label>
+        <br />
+        <br />
+
+        <form action="." method="" onSubmit={this.modify_data}>
+          <fieldset>
+            <legend>
+              <strong>Change Customer Data</strong>
+            </legend>
             <p>
-              <input type="text" onChange={this.myDataChangeHandler} />
+              <label>New Name </label>
+              <input type="text" onChange={this.myNameChangeHandler} />
             </p>
+            <p>
+              <label>New Aadhar </label>
+              <input type="text" onChange={this.myAadharChangeHandler} />
+            </p>
+            <p>
+              <label>New Pan </label>
+              <input type="text" onChange={this.myPanChangeHandler} />
+            </p>
+            <p>
+              <label>New Bank Verify </label>
+              <input type="text" onChange={this.myVBankChangeHandler} />
+            </p>
+            <p>
+              <input type="submit" name="submit" value="Change Data" />
+            </p>
+          </fieldset>
+        </form>
+
+        <br />
+        <br />
+
+        <fieldset>
+          <legend>
+            <strong>Bank Requests</strong>
+          </legend>
+          <p>
+            <button onClick={this.viewRequests}>View Requests</button>
           </p>
-          <button onClick={this.getkycfromcustomer}>Get my Data</button>
-          <p>The stored value is: {this.state.data_hash}</p>
+          <p>
+            <label>Request Address </label>
+            <input type="text" onChange={this.requestAddressChange} />
+          </p>
+
+          <p>
+            <button onClick={this.accept}>Accept Request</button>
+
+            <button onClick={this.reject}>Reject Request</button>
+          </p>
+        </fieldset>
+
+        <br />
+        <br />
+
+        <div>
+          <label>
+            <strong>View Customer data hash</strong>
+          </label>
+          <p>
+            <input type="text" onChange={this.myDataChangeHandler} />
+          </p>
+          <button onClick={this.get}>Get my Data</button>
+          <p>The stored hash value is: {this.state.data_hash}</p>
+        </div>
+
+        <br />
+        <br />
+
+        <div>
+          <label>
+            <strong>View Customer Status</strong>
+          </label>
+          <p>
+            <button onClick={this.getmystatus}>Get Customer Status</button>
+          </p>
+          <p>Customer Status is: {this.state.status}</p>
         </div>
       </div>
     );
--- contracts/KycBlockChain.sol
+++ contracts/KycBlockChain.sol
@@ -2,13 +2,17 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract KycBlockChain{
 
+    address[] public Banks;
+    address[] public Requests;
+    uint public bankslength=0;
+
     enum Entity { Customer, Organisation } 
+    enum Status {Accepted, Rejected, Pending}
 
     struct Customer{
         string c_name;
         string data_hash;
         address bank_address;
-        mapping(address => bool) access;
         bool exists;
         Entity entity;
     }
@@ -17,26 +21,27 @@ contract KycBlockChain{
         string b_name;
         bool exists;
         Entity entity;
+        mapping(address => Status) requests;
+        address[] allrequests;
     }
 
     mapping(address => Customer) allCustomers;
     mapping(address => Organisation) allOrganisations;
 
-    function isOrg() internal view returns(bool){
+    function isOrg() public view returns(bool){
         if(allOrganisations[msg.sender].exists){
             return true;
         }
         return false;
     }
 
-    function isCus() internal view returns(bool){
+    function isCus() public view returns(bool){
         if(allCustomers[msg.sender].exists){
             return true;
         }
         return false;
     }
-    
-    
+
     
     function newCustomer(string memory _name, string memory _hash, address _bank) public payable returns(bool){
         require(!isCus(),"Customer Already Exists!");
@@ -44,9 +49,10 @@ contract KycBlockChain{
         allCustomers[msg.sender].c_name = _name;
         allCustomers[msg.sender].data_hash = _hash;
         allCustomers[msg.sender].bank_address = _bank;
-        allCustomers[msg.sender].access[msg.sender] = true;
+        // allCustomers[msg.sender].access[msg.sender] = true;
         allCustomers[msg.sender].exists = true;
         allCustomers[msg.sender].entity = Entity.Customer;
+        notifyBank(_bank);
         return true;
 
     }
@@ -56,6 +62,8 @@ contract KycBlockChain{
         allOrganisations[msg.sender].b_name = _name;
         allOrganisations[msg.sender].exists = true;
         allOrganisations[msg.sender].entity = Entity.Organisation;
+        Banks.push(msg.sender);
+        bankslength++;
         return true;
     }
 
@@ -66,5 +74,44 @@ contract KycBlockChain{
         }
         return "No such Customer in the database";
     }
+
+    function modifyCustomerData(string memory _name,string memory _hash, address _bank) public payable returns(bool){
+        require(isCus(),"You are not a customer");
+        allCustomers[msg.sender].c_name = _name;
+        allCustomers[msg.sender].data_hash = _hash;
+        allCustomers[msg.sender].bank_address = _bank;
+        return true;
+    }
+
+    function notifyBank(address _bankaddress) internal {
+        allOrganisations[_bankaddress].requests[msg.sender] = Status.Pending;
+        allOrganisations[_bankaddress].allrequests.push(msg.sender);
+    }
+
+    function checkStatus() public returns(Status) {
+        require(isCus(),"You are not a customer");
+        address _presbank = allCustomers[msg.sender].bank_address;
+        return allOrganisations[_presbank].requests[msg.sender];
+    }
+
+    function changeStatusToAccepted(address _custaddress) public payable{
+        require(isOrg(),"You are not permitted to use this function");
+        address _bank = allCustomers[_custaddress].bank_address;
+        require(_bank == msg.sender,"You dont have access to verify this data");
+        allOrganisations[msg.sender].requests[_custaddress] = Status.Accepted;
+    }
+
+    function changeStatusToRejected(address _custaddress) public payable{
+        require(isOrg(),"You are not permitted to use this function");
+        address _bank = allCustomers[_custaddress].bank_address;
+        require(_bank == msg.sender,"You dont have access to verify this data");
+        allOrganisations[msg.sender].requests[_custaddress] = Status.Rejected;
+    }
+
+    function viewRequests() public view returns(address[] memory){
+        require(isOrg(),"You are not Permitted");
+        return allOrganisations[msg.sender].allrequests;
+    }
+
     
 }
